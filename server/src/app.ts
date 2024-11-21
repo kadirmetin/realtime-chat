cd808fe5507e78c8295be6a68e87b11e885422c5
Kadir Metin <kadir-metin@outlook.com.tr>
2024-11-21 20:42:23 +0300

    Main files

---
+++ server/src/app.ts
@@ -0,0 +1,12 @@
+import Express from "express";
+
+const app = Express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
