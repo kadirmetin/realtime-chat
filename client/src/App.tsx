119b1326b3fd1d5ebb384086e1138a8c365def48
Kadir Metin <kadir-metin@outlook.com.tr>
2024-11-22 18:52:34 +0300

    pages added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
 import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SingUpPage";
 
 export default function App() {
   return (
     <MantineProvider>
-      <></>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </MantineProvider>
   );
 }
